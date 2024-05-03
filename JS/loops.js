8fd340faea0fd49b4607b7f846d1484d5f98d9af
harshitagarwal04 <harshitagarwal713@gmail.com>
2024-05-03 23:40:36 +0530

    3rd_may

---
+++ JS/loops.js
@@ -0,0 +1,18 @@
+//for loop
+for (let i = 1; i <= 10; i++) {
+    console.log("2x"+i+"="+2*i);
+}
+
+//while loop
+let j = 1;
+while (j <= 10) {
+    console.log(j);
+    j++;
+}
+
+//do while loop
+let k = 1;
+do {
+    console.log(k);
+    k++;
+} while (k <= 10);
