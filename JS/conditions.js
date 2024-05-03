8b75ee8b3f3462cdc73b8411ab56c80657404c9c
harshitagarwal04 <harshitagarwal713@gmail.com>
2024-05-03 23:30:13 +0530

    2nd_may

---
+++ JS/conditions.js
@@ -0,0 +1,9 @@
+let num = 10;
+
+if (num > 0) {
+    console.log("Number is positive");
+} else if (num < 0) {
+    console.log("Number is negative");
+} else {
+    console.log("Number is zero");
+}
