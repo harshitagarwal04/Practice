b1a7249d4f634b825b3daa683efe32f1436d703f
harshitagarwal04 <harshitagarwal713@gmail.com>
2024-05-04 23:31:48 +0530

    4th_may

---
+++ JS/loops2.js
@@ -0,0 +1,15 @@
+// for in loop
+
+var colors = ["red", "green", "blue"];
+
+for (let color in colors) {
+    console.log((color) + " " + colors[color]);
+}
+
+// for of loop
+
+var scores = [10, 20, 30, 40, 50];  
+
+for (let score of scores) {
+    console.log(score);
+}
